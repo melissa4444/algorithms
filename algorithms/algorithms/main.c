410bfd1fc234accd996cc686a4438b47c54930f6
mestrada7@mail.sfsu.edu <44541773+melissa4444@users.noreply.github.com>
2020-06-09 18:25:49 -0500

    getting started

---
+++ algorithms/algorithms/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  algorithms
+//
+//  Created by Melissa Nguyen on 6/9/20.
+//  Copyright © 2020 Melissa Nguyen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
