8c27b114c650c08330a7e94a144e323919cbcb98
mestrada7@mail.sfsu.edu <44541773+melissa4444@users.noreply.github.com>
2020-06-29 16:28:42 -0500

    wrote a matrix multiplication algorithm

---
+++ algorithms/matrixMult/matrixMult/main.cpp
@@ -0,0 +1,61 @@
+//
+//  main.cpp
+//  matrixMult
+//  add, subtract, multply, and divide matrices
+//  take input for num of row, column num first matrix elements and second matrix elements
+//  then perfrom
+//  Created by Melissa Nguyen on 6/29/20.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[20][20], b[20][20], mul[20][20],m,e,i,j,k;
+    
+    cout << "enter the number of rows = ";
+    cin >> m;
+    
+    cout << "enter the number of columns = ";
+    cin >> e;
+    
+    cout << "enter the first matrix element= \n";
+    for (i =0; i < m; i++){
+        for (j=0; j<e; j++){
+            cin >>a[i][j];
+        }
+    }
+    
+    cout << "enter the second matrix element= \n";
+    for (i=0; i <m; i++){
+        for (j=0; j <e; j++){
+            cin >>b[i][j];
+        }
+    }
+    
+    cout << "multiply the matirx = \n";
+    
+    for (i = 0; i< m; i++){
+        for (j=0; j<e; j++){
+            mul[i][j]=0;
+            for(k=0; k<e; k++){
+                mul[i][j]+=a[i][j]*b[k][j];
+            }
+            
+        }
+    }
+    
+    for (i =0; i<m; i++){
+        for(j=0; j<e; j++){
+            cout << mul[i][j] << "";
+            
+        }
+        cout << "\n";
+    }
+    
+    
+    
+    
+    
+    return 0;
+}
