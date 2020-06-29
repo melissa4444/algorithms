ead5b0dfa70c0a41c2045dc3be984c612a3e6622
mestrada7@mail.sfsu.edu <44541773+melissa4444@users.noreply.github.com>
2020-06-29 15:45:25 -0500

    wrote algorithm

--- algorithms/sequentialSearch/sequentialSearch/main.cpp
+++ algorithms/sequentialSearch/sequentialSearch/main.cpp
@@ -1,14 +1,37 @@
 //
 //  main.cpp
 //  sequentialSearch
-//
+//  simpliest search is applicablt to a table organized either as an array or a linked list
 //  Created by Melissa Nguyen on 6/29/20.
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main() {
+   
+    int array[10];
+    int numOfelements, key;
+    bool found = false;
+    cout << "enter number of elements: ";
+    cin >> numOfelements;
+    
+    for (int i = 0; i < numOfelements; i++){
+        cout << "array[" << i << "]: ";
+        cin >> array[i];
+    }
+    cout << "enter search value: ";
+    cin >> key;
+    for (int i =0; i < numOfelements; i++){
+        if (key== array[i]){
+            found = true;
+            cout << " value found at index array[" << i << "] " ;
+            
+        }
+    }
+    if (!found){
+        cout << "key not found";
+    }
+    
+    
     return 0;
 }
